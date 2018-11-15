b09ff2c123cb7461f3cf9cf132865c17e9c638f8
Lyzhoft <a72383@andersencorp.com>
2018-11-15 13:54:17 -0600

    Home Screen Graphic

---
+++ client/src/components/HomeDockLayout.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div>
+      <div v-for="dock in dockDoors" :key="dock" class="outer-seat" id="div-inline">
+        <h4>{{ dock }}</h4>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeDockLayout",
+  data: () => ({
+    dockDoors: [
+      36,
+      35,
+      34,
+      33,
+      32,
+      31,
+      30,
+      29,
+      28,
+      27,
+      26,
+      25,
+      24,
+      23,
+      22,
+      21,
+      20,
+      19,
+      18,
+      17,
+      16,
+      15,
+      14,
+      13,
+      12,
+      11,
+      10,
+      9,
+      8,
+      7,
+      6,
+      5,
+      4,
+      3,
+      2,
+      1
+    ]
+  })
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h4 {
+  color: black;
+  background: white;
+}
+/* .outer-seat {
+  width: 70px;
+  height: 125px;
+  background: #d8d8d8;
+  position: relative;
+  margin: 4px;
+  margin-top: 2%;
+  text-align: center;
+  color: white;
+} */
+
+#div-inline {
+  float: left;
+  margin-left: 12px;
+}
+
+.clearBoth {
+  clear: both;
+}
+
+.populated:hover,
+.populated:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,44 @@
 <template>
-  <div class="home"><h1>Home</h1></div>
+  <div class="home">
+    <div class="test"></div>
+    <div class="dockLayout">
+      <HomeDockLayout/>
+    </div>
+  </div>
 </template>
 
 <script>
+import HomeDockLayout from "@/components/HomeDockLayout.vue";
+
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    HomeDockLayout
+  }
 };
 </script>
 <style>
+.dockLayout {
+  position: fixed;
+  bottom: 10px;
+  margin: 5px;
+  left: 5px;
+  right: 5px;
+  height: 100px;
+  width: calc(100% - 20px);
+  border: 4px solid black;
+}
+
+.test {
+  height: 100px;
+  width: calc(100% - 20px);
+  margin: 5px;
+  left: 5px;
+  right: 5px;
+  position: fixed;
+  bottom: 111px;
+  border: 4px solid red;
+}
 h1 {
   text-align: center;
 }
